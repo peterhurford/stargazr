aa42bb789beb8c881eba0e3db8157412892988fb
Peter Hurford <peter@peterhurford.com>
2014-06-02 01:58:58 -0400

    Added basic input processing

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,20 @@
 module PagesHelper
+
+	def run_main(zip)
+		if zip.length != 5 || !numeric?(zip)
+			return "Error"
+		else
+			data = 'http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=' + zip
+		end
+		data
+	end
+
+	def numeric?(zip)
+		if zip =~ /^\d+$/
+			return true
+		else
+			false
+		end
+	end
+
 end
